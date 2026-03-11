45a3280d286fc3fc1b73e8de495f84712c0a4b9b
Tran Nhat Tien <trannhattien9801@gmail.com>
2026-03-12 04:24:24 +0700

    update ui

--- server.js
+++ server.js
@@ -1,75 +1,49 @@
-const express = require("express")
-const http = require("http")
-const { Server } = require("socket.io")
-const path = require("path")
-const QRCode = require("qrcode")
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
 
-const app = express()
-const server = http.createServer(app)
-const io = new Server(server)
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/",(req,res)=>{
- res.sendFile(path.join(__dirname,"public","index.html"))
-})
+let players = [];
 
-app.get("/qr", async (req,res)=>{
+io.on("connection", (socket) => {
 
- const ip = req.headers.host
- const url = "http://" + ip
+  socket.on("join", (name) => {
+    players.push({ id: socket.id, name });
+    io.emit("players", players);
+  });
 
- const qr = await QRCode.toDataURL(url)
+  socket.on("start", () => {
 
- res.send(`
-  <html>
-  <body style="text-align:center;font-family:sans-serif">
-  <h2>Scan to Join Game</h2>
-  <img src="${qr}" />
-  <p>${url}</p>
-  </body>
-  </html>
- `)
+    const roles = [
+      "President",
+      "Bomber",
+      "Blue",
+      "Blue",
+      "Red",
+      "Red"
+    ];
 
-})
+    players.forEach((p, i) => {
+      io.to(p.id).emit("role", roles[i % roles.length]);
+    });
 
-let players = []
+  });
 
-const roles = [
- "President",
- "Bomber",
- "Blue Team",
- "Blue Team",
- "Blue Team",
- "Red Team",
- "Red Team",
- "Red Team"
-]
+  socket.on("disconnect", () => {
+    players = players.filter(p => p.id !== socket.id);
+    io.emit("players", players);
+  });
 
-io.on("connection",(socket)=>{
+});
 
- socket.on("join",(name)=>{
-  players.push({id:socket.id,name})
-  io.emit("players",players)
- })
+const PORT = process.env.PORT || 3000;
 
- socket.on("start",()=>{
-
-  const shuffled = roles.sort(()=>Math.random()-0.5)
-
-  players.forEach((p,i)=>{
-   io.to(p.id).emit("role",shuffled[i % shuffled.length])
-  })
-
- })
-
- socket.on("disconnect",()=>{
-  players = players.filter(p=>p.id!==socket.id)
-  io.emit("players",players)
- })
-
-})
-
-server.listen(3000,()=>{
- console.log("Server running")
-})
+server.listen(PORT, () => {
+  console.log("Server running");
+});
