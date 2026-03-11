ef22e0c21efef28492f211593b737cc6743155a9
Tran Nhat Tien <trannhattien9801@gmail.com>
2026-03-12 04:42:23 +0700

    update roles

--- server.js
+++ server.js
@@ -1,49 +1,96 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const path = require("path");
+const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
+const path = require("path")
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname,"public")))
 
-let players = [];
+let players=[]
 
-io.on("connection", (socket) => {
+io.on("connection",(socket)=>{
 
-  socket.on("join", (name) => {
-    players.push({ id: socket.id, name });
-    io.emit("players", players);
-  });
+socket.on("join",(name)=>{
 
-  socket.on("start", () => {
+players.push({
+id:socket.id,
+name:name
+})
 
-    const roles = [
-      "President",
-      "Bomber",
-      "Blue",
-      "Blue",
-      "Red",
-      "Red"
-    ];
+io.emit("players",players)
 
-    players.forEach((p, i) => {
-      io.to(p.id).emit("role", roles[i % roles.length]);
-    });
+})
 
-  });
+socket.on("start",()=>{
 
-  socket.on("disconnect", () => {
-    players = players.filter(p => p.id !== socket.id);
-    io.emit("players", players);
-  });
+let specialRoles=[
 
-});
+"Tổng Thống",
+"Kẻ Đặt Bom",
 
-const PORT = process.env.PORT || 3000;
+"Bác Sĩ",
+"Kỹ Sư",
+"Vệ Sĩ",
+"Thám Tử",
+"Phóng Viên",
+"Luật Sư",
+"Chỉ Huy",
+"Nhà Ngoại Giao",
+"Người Gác Cửa",
 
-server.listen(PORT, () => {
-  console.log("Server running");
-});
+"Gián Điệp",
+"Sát Thủ",
+"Bắn Tỉa",
+"Kẻ Lừa Đảo",
+"Kẻ Phá Hoại",
+
+"Con Bạc",
+"Nhà Đàm Phán",
+"Nhà Tiên Tri",
+"Bản Sao",
+
+"Người Câm Xanh",
+"Người Câm Đỏ"
+
+]
+
+let roles=[...specialRoles]
+
+while(roles.length < players.length){
+
+if(Math.random()>0.5){
+roles.push("Đội Xanh")
+}else{
+roles.push("Đội Đỏ")
+}
+
+}
+
+roles = roles.sort(()=>Math.random()-0.5)
+
+players.forEach((p,i)=>{
+
+io.to(p.id).emit("role",roles[i])
+
+})
+
+})
+
+socket.on("disconnect",()=>{
+
+players = players.filter(p=>p.id!==socket.id)
+
+io.emit("players",players)
+
+})
+
+})
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT,()=>{
+console.log("Server running")
+})
