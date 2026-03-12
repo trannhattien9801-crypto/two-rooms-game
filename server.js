c99e1c42895daa58ef52710f8ac675e584e47d7c
Tran Nhat Tien <trannhattien9801@gmail.com>
2026-03-12 18:21:55 +0700

    update

--- server.js
+++ server.js
@@ -1,101 +1,73 @@
 const express = require("express")
 const http = require("http")
 const { Server } = require("socket.io")
-const path = require("path")
 
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static("public"))
 
 let players = []
 
-io.on("connection",(socket)=>{
+// danh sách vai trò (đã bỏ người đàm phán)
+const roles = [
+"tongthong",
+"boom",
+"bacsi",
+"vesi",
+"thamtu",
+"phongvien",
+"luatsu",
+"kysu",
+"giandiep",
+"satthu",
+"bantia",
+"keluadao",
+"kephahoai",
+"nhatien tri",
+"bansao",
+"conbac",
+"nguoicam",
+"doido",
+"doixanh"
+]
 
-// người chơi tham gia
-socket.on("join",(name)=>{
+io.on("connection", (socket)=>{
 
+console.log("player connected")
+
+socket.on("join", (name)=>{
 players.push({
 id:socket.id,
-name:name
+name:name,
+role:null
 })
 
-io.emit("players",players)
-
+io.emit("updatePlayers",players)
 })
 
-// host bắt đầu game
-socket.on("startGame",(count)=>{
+socket.on("startGame",()=>{
 
-let roles=[]
+let shuffled = [...roles].sort(()=>Math.random()-0.5)
 
-// vai chính
-roles.push("Tổng Thống")
-roles.push("Kẻ Đặt Bom")
-
-// vai đặc biệt
-roles.push("Bác Sĩ")
-roles.push("Kỹ Sư")
-roles.push("Vệ Sĩ")
-roles.push("Thám Tử")
-roles.push("Phóng Viên")
-roles.push("Luật Sư")
-roles.push("Gián Điệp")
-roles.push("Sát Thủ")
-roles.push("Bắn Tỉa")
-roles.push("Kẻ Lừa Đảo")
-roles.push("Kẻ Phá Hoại")
-roles.push("Con Bạc")
-roles.push("Nhà Đàm Phán")
-roles.push("Nhà Tiên Tri")
-roles.push("Bản Sao")
-
-// người câm
-roles.push("Người Câm Xanh")
-roles.push("Người Câm Đỏ")
-
-// thêm dân thường
-while(roles.length < count){
-
-if(Math.random() > 0.5){
-roles.push("Đội Xanh")
-}else{
-roles.push("Đội Đỏ")
-}
-
-}
-
-// random vai
-roles = roles.sort(()=>Math.random()-0.5)
-
-// phát role cho player
 players.forEach((p,i)=>{
+p.role = shuffled[i] || "doixanh"
 
-p.role = roles[i]
+io.to(p.id).emit("yourRole",p.role)
+})
 
-io.to(p.id).emit("role",roles[i])
+io.emit("gameStarted",players)
 
 })
 
-// gửi danh sách role cho host
-io.emit("showRoles",players)
-
-})
-
-// người chơi rời game
 socket.on("disconnect",()=>{
-
 players = players.filter(p=>p.id !== socket.id)
-
-io.emit("players",players)
-
+io.emit("updatePlayers",players)
 })
 
 })
 
-const PORT = process.env.PORT || 3000
-
-server.listen(PORT,()=>{
+server.listen(3000,()=>{
 console.log("Server running")
 })
